6ce72929dc8e5db467698adacc2e0dcfd6305902
Natapon Junjua <natapon.jun01@gmail.com>
2024-11-02 14:56:39 +0700

    initial

--- 2d-top-down/Assets/Scripts/PlayerController.cs
+++ 2d-top-down/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float move_speed = 1;
+    private Rigidbody2D rb;
+    private Vector2 movementDirecttion;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        movementDirecttion = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = movementDirecttion * move_speed;
     }
 }
